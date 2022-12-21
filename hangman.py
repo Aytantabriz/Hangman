10e7024f5b453dc11bef538bcd499d34ef89548c
Aytantabriz <51262587+Aytantabriz@users.noreply.github.com>
2022-12-21 10:39:05 +0400

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,335 @@
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+logo = ''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    '''
+                   
+word_list = [
+'abruptly', 
+'absurd', 
+'abyss', 
+'affix', 
+'askew', 
+'avenue', 
+'awkward', 
+'axiom', 
+'azure', 
+'bagpipes', 
+'bandwagon', 
+'banjo', 
+'bayou', 
+'beekeeper', 
+'bikini', 
+'blitz', 
+'blizzard', 
+'boggle', 
+'bookworm', 
+'boxcar', 
+'boxful', 
+'buckaroo', 
+'buffalo', 
+'buffoon', 
+'buxom', 
+'buzzard', 
+'buzzing', 
+'buzzwords', 
+'caliph', 
+'cobweb', 
+'cockiness', 
+'croquet', 
+'crypt', 
+'curacao', 
+'cycle', 
+'daiquiri', 
+'dirndl', 
+'disavow', 
+'dizzying', 
+'duplex', 
+'dwarves', 
+'embezzle', 
+'equip', 
+'espionage', 
+'euouae', 
+'exodus', 
+'faking', 
+'fishhook', 
+'fixable', 
+'fjord', 
+'flapjack', 
+'flopping', 
+'fluffiness', 
+'flyby', 
+'foxglove', 
+'frazzled', 
+'frizzled', 
+'fuchsia', 
+'funny', 
+'gabby', 
+'galaxy', 
+'galvanize', 
+'gazebo', 
+'giaour', 
+'gizmo', 
+'glowworm', 
+'glyph', 
+'gnarly', 
+'gnostic', 
+'gossip', 
+'grogginess', 
+'haiku', 
+'haphazard', 
+'hyphen', 
+'iatrogenic', 
+'icebox', 
+'injury', 
+'ivory', 
+'ivy', 
+'jackpot', 
+'jaundice', 
+'jawbreaker', 
+'jaywalk', 
+'jazziest', 
+'jazzy', 
+'jelly', 
+'jigsaw', 
+'jinx', 
+'jiujitsu', 
+'jockey', 
+'jogging', 
+'joking', 
+'jovial', 
+'joyful', 
+'juicy', 
+'jukebox', 
+'jumbo', 
+'kayak', 
+'kazoo', 
+'keyhole', 
+'khaki', 
+'kilobyte', 
+'kiosk', 
+'kitsch', 
+'kiwifruit', 
+'klutz', 
+'knapsack', 
+'larynx', 
+'lengths', 
+'lucky', 
+'luxury', 
+'lymph', 
+'marquis', 
+'matrix', 
+'megahertz', 
+'microwave', 
+'mnemonic', 
+'mystify', 
+'naphtha', 
+'nightclub', 
+'nowadays', 
+'numbskull', 
+'nymph', 
+'onyx', 
+'ovary', 
+'oxidize', 
+'oxygen', 
+'pajama', 
+'peekaboo', 
+'phlegm', 
+'pixel', 
+'pizazz', 
+'pneumonia', 
+'polka', 
+'pshaw', 
+'psyche', 
+'puppy', 
+'puzzling', 
+'quartz', 
+'queue', 
+'quips', 
+'quixotic', 
+'quiz', 
+'quizzes', 
+'quorum', 
+'razzmatazz', 
+'rhubarb', 
+'rhythm', 
+'rickshaw', 
+'schnapps', 
+'scratch', 
+'shiv', 
+'snazzy', 
+'sphinx', 
+'spritz', 
+'squawk', 
+'staff', 
+'strength', 
+'strengths', 
+'stretch', 
+'stronghold', 
+'stymied', 
+'subway', 
+'swivel', 
+'syndrome', 
+'thriftless', 
+'thumbscrew', 
+'topaz', 
+'transcript', 
+'transgress', 
+'transplant', 
+'triphthong', 
+'twelfth', 
+'twelfths', 
+'unknown', 
+'unworthy', 
+'unzip', 
+'uptown', 
+'vaporize', 
+'vixen', 
+'vodka', 
+'voodoo', 
+'vortex', 
+'voyeurism', 
+'walkway', 
+'waltz', 
+'wave', 
+'wavy', 
+'waxy', 
+'wellspring', 
+'wheezy', 
+'whiskey', 
+'whizzing', 
+'whomever', 
+'wimpy', 
+'witchcraft', 
+'wizard', 
+'woozy', 
+'wristwatch', 
+'wyvern', 
+'xylophone', 
+'yachtsman', 
+'yippee', 
+'yoked', 
+'youthful', 
+'yummy', 
+'zephyr', 
+'zigzag', 
+'zigzagging', 
+'zilch', 
+'zipper', 
+'zodiac', 
+'zombie', 
+]
+
+import random
+
+#from hangman_words import word_list
+
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word) #seçilən sözdə hərflərin sayı
+
+end_of_game = False
+lives = 6
+
+#from hangman_art import logo
+print(logo)
+
+# print(f'Pssst, the solution is {chosen_word}.')
+
+display = []
+for _ in range(word_length):
+    display += "_" # seçilən sözdəki hərflərin sayı qədər _ əlavə edir.
+
+#MAIN LOOP
+while not end_of_game: #end_of_game=TRUE olana qədər loopu davam etdirir.
+    guess = input("Guess a letter: ").lower() #yeni hərf soruşur.
+
+    #Artıq daxil edilmiş hərfi bir daha daxil etdikdə
+    if guess in display: # əgər displaydə öncədən daxil etdiyimiz dəyişəni yenidən daxil ediriksə icra edir.
+        print(f"You've already guessed {guess}")
+
+    #Daxil etdiyimiz hərf seçilən sözün içində olduqda
+    for position in range(word_length): #seçilən sözün hərflərin yerini daxil etdiyimiz dəyişənlərlə yoxlayır.
+        letter = chosen_word[position]
+        #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess: # əgər hərflər eynidirsə,
+            display[position] = letter # _ yerinə daxil etdiyimiz hərfi göstərsin.
+
+    #Daxil etdiyimiz hərf seçilən sözün içində olmadıqda
+    if guess not in chosen_word: # əgər yazdığımız hərf seçilən sözün içində yoxdursa, icra edir.
+        #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        
+        lives -= 1 # canı 1 vahid azaldır.
+        if lives == 0: # əgər canımız artıq 0-dırsa,
+            end_of_game = True # oyunun bitməsi üçün loopu bağlayır.
+            print("You lose.")
+
+    print(f"{' '.join(display)}") # displaydə olan dəyişənləri birləşdirir.
+
+    if "_" not in display: # əgər displaydə _ yoxdursa qalib kimi loopu sonlandırır.
+        end_of_game = True
+        print("You win.")
+
+    #from hangman_art import stages
+    print(stages[lives]) # canlarımızın sayına uyğun olaraq stagesləri konsola çıxarır.
